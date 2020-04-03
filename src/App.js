bf89ca023f68daa5ea3d7f4ea06782e646c80968
Vasil Todorovski <vasil.todorovski@symphony-solutions.eu>
2020-04-03 18:11:12 +0200

    Moved mock data to a separate file

--- src/App.js
+++ src/App.js
@@ -1,61 +1,10 @@
 import React from 'react';
 import './App.css';
 import NestedList from './components/NestedList';
+import { data } from './utils/generateData';
 
 function App() {
-    let data = [
-        {
-        id: 115123425,
-        text: "element1",
-        children: [{
-            id: 115345425,
-            text: "subelement1.1",
-            children: [{
-                id: 1151343225,
-                text: "subelement1.1.1"
-                }, {
-                id: 2351343225,
-                text: "subelement1.1.2"
-                }
-            ]
-            }, {
-            id: 5855843225,
-            text: "subelement1.2"
-            }
-        ]
-        }, {
-        id: 17651343225,
-        text: "element2",
-        children: [{
-            id: 1151343125,
-            text: "subelement2.1"
-            }, {
-            id: 1155256262,
-            text: "subelement2.2",
-            children: [{
-                id: 1151378225,
-                text: "subelement2.2.1"
-                }, {
-                id: 1151093225,
-                text: "subelement2.2.2",
-                children: [{
-                    id: 1151378226,
-                    text: "subelement2.2.2.1"
-                }, {
-                    id: 1151093227,
-                    text: "subelement2.2.2.2"
-                }
-                ]
-                }
-            ]
-            }
-        ]
-        }
-    ];
-
-    return (
-        <NestedList data={data}/>
-    );
+    return <NestedList data={data} />;
 }
 
 export default App;
---
+++ src/utils/generateData.js
@@ -0,0 +1,88 @@
+export const data = [
+    {
+        id: 115123425,
+        text: 'element1',
+        children: [
+            {
+                id: 115345425,
+                text: 'subelement1.1',
+                children: [
+                    {
+                        id: 1151343225,
+                        text: 'subelement1.1.1'
+                    },
+                    {
+                        id: 2351343225,
+                        text: 'subelement1.1.2'
+                    }
+                ]
+            },
+            {
+                id: 5855843225,
+                text: 'subelement1.2'
+            }
+        ]
+    },
+    {
+        id: 17651343225,
+        text: 'element2',
+        children: [
+            {
+                id: 1151343125,
+                text: 'subelement2.1'
+            },
+            {
+                id: 1155256262,
+                text: 'subelement2.2',
+                children: [
+                    {
+                        id: 1151378225,
+                        text: 'subelement2.2.1'
+                    },
+                    {
+                        id: 1151093225,
+                        text: 'subelement2.2.2',
+                        children: [
+                            {
+                                id: 1151378226,
+                                text: 'subelement2.2.2.1'
+                            },
+                            {
+                                id: 1151093227,
+                                text: 'subelement2.2.2.2'
+                            }
+                        ]
+                    },
+                    {
+                        id: 1151093226,
+                        text: 'subelement2.2.3',
+                        children: [
+                            {
+                                id: 1151234155,
+                                text: 'subelement2.2.3.1'
+                            },
+                            {
+                                id: 11510967999,
+                                text: 'subelement2.2.3.2'
+                            }
+                        ]
+                    },
+                    {
+                        id: 11312141515,
+                        text: 'subelement2.2.4',
+                        children: [
+                            {
+                                id: 1156585685,
+                                text: 'subelement2.2.4.1'
+                            },
+                            {
+                                id: 1574574227,
+                                text: 'subelement2.2.4.2'
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+];
